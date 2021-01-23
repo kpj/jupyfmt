18592b2c11556c2374a760bb30bd6a5d75fbde85
kpj <kim.philipp.jablonski@gmail.com>
2021-01-23 23:53:37 +0100

    Improve final report

--- jupyfmt/main.py
+++ jupyfmt/main.py
@@ -16,7 +16,9 @@ def format_file(notebook_path: PathLike, mode: black.FileMode):
     with open(notebook_path) as fd:
         nb = nbf.read(fd, as_version=4)
 
+    cells_errored = 0
     cells_changed = 0
+    cells_unchanged = 0
     for i, cell in enumerate(nb['cells']):
         if cell['cell_type'] != 'code':
             continue
@@ -31,6 +33,8 @@ def format_file(notebook_path: PathLike, mode: black.FileMode):
             fmted_source = black.format_str(orig_source, mode=mode)
         except black.InvalidInput as e:
             print(f'Error "{str(e)}" while formatting code with black.')
+            cells_errored += 1
+            continue
 
         if orig_source != fmted_source:
             header = f'{notebook_path} - Cell {i} '
@@ -47,8 +51,10 @@ def format_file(notebook_path: PathLike, mode: black.FileMode):
             print(diff_str)
 
             cells_changed += 1
+        else:
+            cells_unchanged += 1
 
-    return cells_changed
+    return cells_errored, cells_changed, cells_unchanged
 
 
 def get_notebooks_in_dir(path):
@@ -89,6 +95,7 @@ def main(
     skip_string_normalization: bool,
     path_list: List[PathLike],
 ):
+    # gather files to format
     files_to_format = set()
     for path in path_list:
         path = Path(path)
@@ -98,17 +105,40 @@ def main(
         elif path.is_dir():
             files_to_format.update(get_notebooks_in_dir(path))
 
+    # format files
     mode = black.FileMode(
         line_length=line_length, string_normalization=not skip_string_normalization
     )
 
+    files_errored = 0
     files_changed = 0
+    files_unchanged = 0
     for notebook_fname in files_to_format:
-        if format_file(notebook_fname, mode):
-            files_changed += 1
+        cells_errored, cells_changed, cells_unchanged = format_file(notebook_fname, mode)
 
-    if files_changed:
-        ctx.exit(1)
+        if cells_errored > 0:
+            files_errored += 1
+        elif cells_changed > 0:
+            files_changed += 1
+        else:
+            files_unchanged += 1
+
+    # report
+    if files_errored > 0:
+        print(f'{files_errored} file(s) raised parsing errors 🤕')
+    if files_changed > 0:
+        print(f'{files_changed} file(s) would be changed 😬')
+    if files_unchanged > 0:
+        print(f'{files_unchanged} file(s) would be left unchanged 🎉')
+
+    # return appropriate exit code
+    exit_code = 0
+    if files_errored > 0:
+        exit_code = 2
+    elif files_changed > 0:
+        exit_code = 1
+
+    ctx.exit(exit_code)
 
 
 if __name__ == '__main__':
